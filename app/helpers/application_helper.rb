7aa57228b774de7b066797855d17ef604d269e89
dohalong <dohalong1993@gmail.com>
2017-01-04 10:07:46 +0700

    Static pages added + I18n js supported

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "title"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
+
+  def render_404
+    render file: "public/404.html", layout: false, status: 404
+  end
+
+  def active_class link_path
+    current_page?(link_path) ? "active" : ""
+  end
+
+  def active_class_locale locale
+    locale == I18n.locale ? "active" : ""
+  end
 end
