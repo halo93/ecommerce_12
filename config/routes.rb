d8f24ca52a1f1a08901d60d4bd31dab6023268de
Nguyen Van Duc <nguyen.van.ducb@framgia.com>
2017-01-04 18:33:33 +0700

    frontend admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vn|jp/ do
+    namespace :admin do
+      root "categories#index"
+      resources :categories
+      resources :products
+    end
     get "/pages/*page" => "pages#show"
     root "pages#home"
   end
