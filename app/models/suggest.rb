caa12159d6b0c164d7821469bbb123f8d7776d7f
dohalong <dohalong1993@gmail.com>
2017-01-09 14:40:49 +0700

    User make suggestion part 2: support ransack, kaminari, faker, simple form

--- app/models/suggest.rb
+++ app/models/suggest.rb
@@ -1,4 +1,15 @@
 class Suggest < ApplicationRecord
   belongs_to :user
-  belongs_to :category
+  belongs_to :category, optional: true
+
+  validates :title, presence: true
+
+  before_create :init_suggest_status
+
+  enum status: [:processing, :accepted, :rejected]
+
+  private
+  def init_suggest_status
+    self[:status] = :processing
+  end
 end
