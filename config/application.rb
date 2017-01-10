048f5dee32d511fbf82216dca9a8f18902d3a717
dohalong <dohalong1993@gmail.com>
2017-01-10 10:00:22 +0700

    Fix bug and modify UI

--- config/application.rb
+++ config/application.rb
@@ -8,5 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Ecommerce12
   class Application < Rails::Application
+    config.i18n.load_path += Dir[Rails.root.join("config",
+      "locales", "**", "*.{rb,yml}").to_s]
   end
 end
